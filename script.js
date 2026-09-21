e62a506abdb59bb9336fe9dacb491461727e2594
hibaessadraoui-cloud <hibaessadraoui@gmail.com>
2026-09-21 13:48:31 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-let danon = prompt("nom de l'article");
-let prix = prompt("prix");
-let quantite = prompt("quantite");
-let total = prix * quantite;
-console.log(danon);
-console.log(prix);
-console.log(quantite);
-console.log(total);
